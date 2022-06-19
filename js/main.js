06cd5e2463a73cbb47e0cce30df6f83c03dbeedf
plan-03 <PLAN-03@IEMCO-GARMENTS.COM>
2022-06-19 14:43:20 +0200

    Create Egyptian party project

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+$(".toggle-menu-open , .close-icon").click(function () {
+    let widthVal = $("nav").outerWidth();
+    let leftVal = $("nav").css("left");
+    if(leftVal == "0px") {
+        $("nav").animate({left: `-${widthVal}px`},700);
+    }else {
+        $("nav").animate({left: `0`},700);
+    }
+})
+
+$(".singers ul li h3").click(function () {
+    $(this).next().slideToggle(1000).parent().siblings().find("p").slideUp(1000)
+})
+
+let countDown = new Date("7-18-2022").getTime();
+let counter = setInterval(()=>{
+    let timeNow = new Date().getTime();
+    let diffrentTime = countDown - timeNow;
+    let days = Math.floor(diffrentTime /1000 / 60 / 60 /24);
+    let hours = Math.floor(diffrentTime % ((1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor(diffrentTime % ((1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor(diffrentTime % ((1000 * 60)) / 1000);
+    $("#days").html(`- ${days} D`);
+    $("#hours").html(`${hours} h`);
+    $("#minutes").html(`${minutes} m`);
+    $("#seconds").html(`${seconds} s`);
+},1000)
+
+$("textarea").keyup(function () {
+    let total = 99;
+    let totalValue = $("textarea").val().split('');
+    for (let i = -1; i < totalValue.length; i++) {
+        if (totalValue.length <= total) {
+            $(".character span").html(`${total - i}`)
+        }else {
+            $(".character span").html(`your available character finished`)
+        }
+        
+    }
+})
+$("nav a[href^='#']").click(function (e) {
+    let linkHref = $(e.target).attr('href');
+    let sectionOffset = $(linkHref).offset().top;
+    $("html,body").animate({scrollTop:sectionOffset} , 3000);
+})
